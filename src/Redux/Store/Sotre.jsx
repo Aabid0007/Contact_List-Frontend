43651186b75ad66d17b43a3022fc14982ce1211b
Aabid <zainulaabid0007@gmail.com>
2024-07-01 21:17:36 +0530

    Updates

--- src/Redux/Store/Sotre.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import GetDataReducer from "../Reducer/GetData.Slice";
-
- const store = configureStore({
-    reducer: {
-        data: GetDataReducer,
-    },
-})
-export default store;
